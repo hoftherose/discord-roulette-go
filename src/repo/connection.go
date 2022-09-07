670650ef906dbe8cdecb5cef51a04d4b444bd2b8
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-09-06 23:18:48 -0500

    Parameterize db connection

--- src/repo/connection.go
+++ src/repo/connection.go
@@ -2,6 +2,8 @@ package repo
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	u "github.com/holy-tech/discord-roulette/src"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -9,7 +11,12 @@ import (
 )
 
 var Client *mongo.Client
-var uri = "mongodb://root:root@localhost:27017"
+var uri = fmt.Sprintf("mongodb://%s:%s@%s:%s",
+	os.Getenv("DB_USER"),
+	os.Getenv("DB_PASS"),
+	os.Getenv("DB_IP"),
+	os.Getenv("DB_PORT"),
+)
 
 func init() {
 	var err error
