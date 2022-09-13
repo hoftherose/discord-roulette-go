c23a74d08f20326dd643af137790ed63c9d99092
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-09-12 23:28:59 -0500

    Objectify mongo update (no compile)

--- src/repo/players.go
+++ src/repo/players.go
@@ -1,20 +1,30 @@
 package repo
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"errors"
+
+	d "github.com/holy-tech/discord-roulette/src/data"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func AcceptPlayer(channel string, user string) error {
+	var result d.GameSettings
 	if GameIsAcceptedBy(channel, user) {
 		return errors.New("you have already accepted")
 	}
 
-	// db := Client.Database("games")
-	// gameCollection := db.Collection(fmt.Sprintf("%s_game", channel))
-	// ctx, cancelCtx := context.WithTimeout(context.Background(), 5*time.Second)
-	// defer cancelCtx()
+	db := Client.Database("games")
+	gameCollection := db.Collection(fmt.Sprintf("%s_game", channel))
+	ctx, cancelCtx := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelCtx()
 
-	// result, _ := gameCollection.UpdateOne(ctx, bson.M{"channel": channel}, bson.D{{"opponents": [{"accepted": "true"}]}})
-	// fmt.Println(result)
+	gameCollection.FindOne(ctx, bson.M{"channel": channel}).Decode(&result)
+	update, _ := gameCollection.UpdateOne(ctx, bson.M{"channel": channel}, result)
+	fmt.Println(result)
+	fmt.Println(update)
 	return nil
 }
