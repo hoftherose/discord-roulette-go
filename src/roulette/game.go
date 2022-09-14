9e8174022a59d53f95850d4a56553a55ae44f558
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-09-13 23:08:34 -0500

    Init shooting mechanics

--- src/roulette/game.go
+++ src/roulette/game.go
@@ -1,12 +1,16 @@
 package roulette
 
 import (
+	"math/rand"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/holy-tech/discord-roulette/src/data"
 	db "github.com/holy-tech/discord-roulette/src/repo"
 )
 
-func Died() bool {
-	return true
+func Shoot(s data.GameSettings) (bool, error) {
+	died := rand.Intn(int(s.GunState.NumChamber)) > int(s.GunState.NumBullets)
+	return died, nil
 }
 
 func ShootTurn(channel string, user *discordgo.User) string {
@@ -14,9 +18,12 @@ func ShootTurn(channel string, user *discordgo.User) string {
 	if err != nil {
 		return "No shots fired: " + err.Error()
 	}
+	settings, _ := db.GetGameDocument(channel)
 	if !accepted {
 		return "Game still is not accepted"
-	} else if Died() {
+	}
+	died, _ := Shoot(settings)
+	if died {
 		return "You died <@" + user.ID + ">"
 	}
 	return "You live <@" + user.ID + ">"
