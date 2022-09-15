5ac45d0f2c3fce3f09abe514c5da7bedaa05de4b
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-09-14 22:53:43 -0500

    Mixed up file names

--- src/data/gun_state.go
+++ src/data/gun_state.go
@@ -2,39 +2,38 @@ package data
 
 import (
 	"math/rand"
-	"time"
 )
 
 var (
-	DefaultLosers      []bool   = []bool{}
-	DefaultTurns       []string = []string{}
-	DefaultCurrentTurn int      = 0
+	DefaultChamber        []bool = []bool{}
+	DefaultNumChamber     int    = 6
+	DefaultNumBullet      int    = 1
+	DefaultCurrentChamber int    = 0
 )
 
-type TableState struct {
-	Losers      []bool   `json:"losers"`
-	Turns       []string `json:"turns"`
-	CurrentTurn int      `json:"current_turn"`
+type GunState struct {
+	Chambers       []bool `json:"chambers"`
+	NumChamber     int    `json:"num_chambers"`
+	NumBullets     int    `json:"num_bullets"`
+	CurrentChamber int    `json:"current_chamber"`
 }
 
-var DefaultTableState TableState = TableState{
-	DefaultLosers,
-	DefaultTurns,
-	DefaultCurrentTurn,
+var DefaultGunState GunState = GunState{
+	DefaultChamber,
+	DefaultNumChamber,
+	DefaultNumBullet,
+	DefaultCurrentChamber,
 }
 
-func (g *GunState) SetNextChamber() {
-	curr_chamber := g.CurrentChamber
-	g.CurrentChamber = (curr_chamber + 1) % g.NumChamber
+func (t *TableState) GetCurrentPlayer() string {
+	curr_player := t.CurrentTurn
+	return t.Turns[curr_player]
 }
 
-func (g *GunState) SpinChamber() {
-	g.Chambers = make([]bool, g.NumChamber)
-	for k := 0; k < g.NumChamber; k++ {
-		g.Chambers[k] = k < g.NumBullets
-	}
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(g.Chambers), func(i, j int) {
-		g.Chambers[i], g.Chambers[j] = g.Chambers[j], g.Chambers[i]
-	})
+func (t *TableState) SetNextPlayer() {
+	t.CurrentTurn = (t.CurrentTurn + 1) % len(t.Turns)
+}
+
+func (t *TableState) SpinTable() {
+	rand.Shuffle(len(t.Turns), func(i, j int) { t.Turns[i], t.Turns[j] = t.Turns[j], t.Turns[i] })
 }
