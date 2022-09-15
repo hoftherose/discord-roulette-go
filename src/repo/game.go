de2398d0d3e610e69e5b811f62c9ef5b9b3f9d7d
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-09-14 22:52:22 -0500

    Move CRUD document functions

--- src/repo/game.go
+++ src/repo/game.go
@@ -1,77 +1,10 @@
 package repo
 
 import (
-	"context"
 	"errors"
-	"fmt"
-	"time"
-
-	"log"
 
 	"github.com/bwmarrin/discordgo"
-	d "github.com/holy-tech/discord-roulette/src/data"
-	"go.mongodb.org/mongo-driver/bson"
 )
-
-func CreateGameDocument(channel string, settings interface{}) error {
-	var result bson.M
-	db := Client.Database("games")
-	gameCollection := db.Collection(fmt.Sprintf("%s_game", channel))
-	ctx, cancelCtx := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelCtx()
-
-	gameCollection.FindOne(ctx, bson.D{}).Decode(&result)
-
-	if result != nil {
-		return errors.New("game already exists")
-	}
-	_, err := gameCollection.InsertOne(ctx, settings)
-	if err != nil {
-		log.Fatalf("Error executing query: %v\n", err)
-	}
-	return err
-}
-
-func DeleteGameDocument(channel string) error {
-	db := Client.Database("games")
-	gameCollection := db.Collection(fmt.Sprintf("%s_game", channel))
-	ctx, cancelCtx := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelCtx()
-
-	result, err := gameCollection.DeleteOne(ctx, bson.D{{}})
-	if result.DeletedCount != 1 {
-		return errors.New("no game is currently ongoing")
-	}
-	return err
-}
-
-func GetGameDocument(channel string) (d.GameSettings, error) {
-	var result d.GameSettings
-	db := Client.Database("games")
-	gameCollection := db.Collection(fmt.Sprintf("%s_game", channel))
-	ctx, cancelCtx := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelCtx()
-
-	enc_result := gameCollection.FindOne(ctx, bson.M{"channel": channel})
-	enc_result.Decode(&result)
-	return result, enc_result.Err()
-}
-
-func UpdateGameDocument(channel string, new interface{}) error {
-	db := Client.Database("games")
-	gameCollection := db.Collection(fmt.Sprintf("%s_game", channel))
-	ctx, cancelCtx := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelCtx()
-
-	update, err := gameCollection.ReplaceOne(ctx, bson.M{"channel": channel}, new)
-
-	if err != nil {
-		return err
-	} else if update.ModifiedCount == 0 {
-		return errors.New("no update occured")
-	}
-	return nil
-}
 
 func AcceptGame(channel string) error {
 	result, _ := GetGameDocument(channel)
