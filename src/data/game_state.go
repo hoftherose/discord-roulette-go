095efec265d5e3ae9d4abcb5bb2f8efa1355cd39
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-09-14 22:01:06 -0500

    Add current state table and gun

--- src/data/game_state.go
+++ src/data/game_state.go
@@ -1,32 +1,38 @@
 package data
 
 var (
-	DefaultLosers []bool   = []bool{}
-	DefaultTurns  []string = []string{}
+	DefaultLosers      []bool   = []bool{}
+	DefaultTurns       []string = []string{}
+	DefaultCurrentTurn int64    = 0
 
-	DefaultChamber    []bool = []bool{}
-	DefaultNumChamber int64  = 6
-	DefaultNumBullet  int64  = 1
+	DefaultChamber        []bool = []bool{}
+	DefaultNumChamber     int64  = 6
+	DefaultNumBullet      int64  = 1
+	DefaultCurrentChamber int64  = 0
 )
 
 type TableState struct {
-	Losers []bool   `json:"losers"`
-	Turns  []string `json:"current_turn"`
+	Losers      []bool   `json:"losers"`
+	Turns       []string `json:"turns"`
+	CurrentTurn int64    `json:"current_turn"`
 }
 
 type GunState struct {
-	Chambers   []bool `json:"chambers"`
-	NumChamber int64  `json:"num_chambers"`
-	NumBullets int64  `json:"num_bullets"`
+	Chambers       []bool `json:"chambers"`
+	NumChamber     int64  `json:"num_chambers"`
+	NumBullets     int64  `json:"num_bullets"`
+	CurrentChamber int64  `json:"current_chamber"`
 }
 
 var DefaultTableState TableState = TableState{
 	DefaultLosers,
 	DefaultTurns,
+	DefaultCurrentTurn,
 }
 
 var DefaultGunState GunState = GunState{
 	DefaultChamber,
 	DefaultNumChamber,
 	DefaultNumBullet,
+	DefaultCurrentChamber,
 }
