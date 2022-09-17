31036f61f88a932d9d732ccc67838d2cf1fb6f6a
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-09-16 23:05:56 -0500

    Use item

--- src/data/items.go
+++ src/data/items.go
@@ -1,8 +1,15 @@
 package data
 
+import "fmt"
+
 type Item struct {
 	Name        string
 	Description string
 	Inventory   int
-	Effect      func(GameSettings)
+	Effect      func(*GameSettings)
+}
+
+func (i *Item) Use(s *GameSettings) string {
+	i.Effect(s)
+	return i.Name + " was used, " + fmt.Sprint(i.Inventory) + " uses left"
 }
