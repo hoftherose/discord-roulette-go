0bfec67a46937434021c41b27968b519acd8c5f1
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-10-03 12:00:23 -0500

    Test Game Status

--- src/data/game_test.go
+++ src/data/game_test.go
@@ -4,33 +4,78 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/golang/mock/gomock"
+	mock "github.com/holy-tech/discord-roulette/mocks"
 	"github.com/holy-tech/discord-roulette/src/data"
 )
 
-func TestShoot(t *testing.T) {
-	var tests = []struct {
-		currPlayer, currChamber int
-		expected                bool
-		expectedErr             error
-	}{
-		{6, 0, true, nil},
-		{6, 4, true, nil},
-		{6, 5, true, nil},
-		{7, 5, true, nil},
-		{1, 0, true, nil},
+func PopulatePlayersAccept(ctrl *gomock.Controller, accepted []bool) []data.User {
+	list := []data.User{}
+	for _, accept := range accepted {
+		newUser := mock.NewMockUser(ctrl)
+		newUser.EXPECT().HasAccepted().Return(accept).AnyTimes()
+		list = append(list, newUser)
 	}
+	return list
+}
 
-	for _, tt := range tests {
-		testname := fmt.Sprintf("%d,%d", tt.currPlayer, tt.currChamber)
+func TestIsAccepted(t *testing.T) {
+	var tests = []struct {
+		acceptance []bool
+		expected   bool
+	}{
+		{[]bool{true, false}, false},
+		{[]bool{false, false, false}, false},
+		{[]bool{true, true}, true},
+		{[]bool{true}, true},
+		{[]bool{}, true},
+	}
+	for i, tt := range tests {
+		testname := fmt.Sprintf("game_finished_%d", i)
 		t.Run(testname, func(t *testing.T) {
-			gsetting := data.GameStatus{}
-			shot, err := gsetting.Shoot(&discordgo.User{})
-			if shot != tt.expected {
-				t.Errorf("got %t, want %t", shot, tt.expected)
+			mockCtrl := gomock.NewController(t)
+			table := mock.NewMockTable(mockCtrl)
+			players := PopulatePlayersAccept(
+				mockCtrl,
+				tt.acceptance,
+			)
+			table.EXPECT().
+				GetSeating().
+				Return(
+					players,
+				)
+			gun := mock.NewMockGun(mockCtrl)
+			game := data.GameStatus{table, gun, false, ""}
+			finished := game.IsAccepted()
+			if finished != tt.expected {
+				t.Errorf("got %t, want %t", finished, tt.expected)
 			}
-			if err.Error() != tt.expectedErr.Error() {
-				t.Errorf("got %s, want %s", err.Error(), tt.expectedErr.Error())
+		})
+	}
+}
+
+func TestGameFinished(t *testing.T) {
+	var tests = []struct {
+		numPlayers int
+		expected   bool
+	}{
+		{6, false},
+		{3, false},
+		{1, true},
+		{0, true},
+		{-1, true},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("game_finished_%d", tt.numPlayers)
+		t.Run(testname, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			table := mock.NewMockTable(mockCtrl)
+			table.EXPECT().NumPlayers().Return(tt.numPlayers)
+			gun := mock.NewMockGun(mockCtrl)
+			game := data.GameStatus{table, gun, false, ""}
+			finished := game.GameFinished()
+			if finished != tt.expected {
+				t.Errorf("got %t, want %t", finished, tt.expected)
 			}
 		})
 	}
