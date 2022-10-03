ab8d51ba23d5bfdef5497d853ca09021850e0fba
Héctor de la Rosa Prado <hoftherose@gmail.com>
2022-10-03 12:32:28 -0500

    Test init table

--- src/data/game_table_test.go
+++ src/data/game_table_test.go
@@ -4,28 +4,43 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	mock "github.com/holy-tech/discord-roulette/mocks"
 	"github.com/holy-tech/discord-roulette/src/data"
 )
 
-func TestGetCurrentPlayer(t *testing.T) {
+func PopulatePlayersID(ctrl *gomock.Controller, names []string) []data.User {
+	list := []data.User{}
+	for _, name := range names {
+		newUser := mock.NewMockUser(ctrl)
+		newUser.EXPECT().GetID().Return(name).AnyTimes()
+		list = append(list, newUser)
+	}
+	return list
+}
+
+func TestInitTable(t *testing.T) {
 	var tests = []struct {
-		currTurn int
-		turns    []string
-		expected string
+		playerNames []string
+		seed        int64
+		expected    []string
 	}{
-		{0, []string{"hello", "world"}, "hello"},
-		{1, []string{"hello", "world"}, "world"},
+		{[]string{"hello", "world"}, 42, []string{"hello", "world"}},
+		{[]string{"1", "2", "3"}, 42, []string{"1", "2", "3"}},
+		{[]string{"1", "2", "3", "4", "5"}, 42, []string{"4", "1", "5", "2", "3"}},
 	}
 
-	for _, tt := range tests {
-		testname := fmt.Sprintf("%d,%v", tt.currTurn, tt.turns)
+	for i, tt := range tests {
+		testname := fmt.Sprintf("%d", i)
 		t.Run(testname, func(t *testing.T) {
-			gstate := data.TableState{
-				CurrentTurn: tt.currTurn,
-				Turns:       tt.turns,
-			}
-			if gstate.GetCurrentPlayer() != tt.expected {
-				t.Errorf("got %s, want %s", gstate.GetCurrentPlayer(), tt.expected)
+			mockCtrl := gomock.NewController(t)
+			table := data.GameTable{}
+			players := PopulatePlayersID(mockCtrl, tt.playerNames)
+			table.InitTable(players...)
+			for i, name := range table.GetSeating() {
+				if name.GetID() != tt.expected[i] {
+					t.Errorf("got %s, want %s", name.GetID(), tt.expected[i])
+				}
 			}
 		})
 	}
